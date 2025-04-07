b74b6f883805bbd1693f8dd67f78da75122bcf1b
dusixx <113414935+dusixx@users.noreply.github.com>
2025-04-07 10:47:40 +0300

    feat: implement Winners

---
+++ async-race/src/sections/winners/utils/create-view.ts
@@ -0,0 +1,135 @@
+import { div, li, span, ul } from '../../../components/base/tags.ts';
+import { Car } from '../../../components/car/car.ts';
+import type { CarViewType } from '../../../components/car/utils/misc.ts';
+import { Paginator } from '../../../components/paginator/paginator.ts';
+import { SortableColumn } from '../../../components/sortable-column/sortable-column.ts';
+import type { CarData, WinnerData } from '../../../services/api/types.ts';
+import styles from '../winners.module.scss';
+
+const TOTAL_WINNERS_TEXT = 'winners total:';
+
+enum HeaderColumnText {
+  Index = '#',
+  Car = 'car',
+  Name = 'name',
+  Wins = 'wins',
+  BestTime = 'best time',
+}
+
+type TableViewReturnType = {
+  winsColumn: SortableColumn;
+  timeColumn: SortableColumn;
+  tableContentWrapper: ReturnType<typeof div>;
+  tableWrapper: ReturnType<typeof div>;
+};
+
+type CreateViewReturnType = Omit<TableViewReturnType, 'tableWrapper'> & {
+  wrapper: ReturnType<typeof div>;
+  paginator: Paginator;
+  winnersCounter: ReturnType<typeof span>;
+};
+
+export type WinnerTableRowData = WinnerData & CarData & { type?: CarViewType };
+
+export const createWinnersTableContent = (data: WinnerTableRowData[]): ReturnType<typeof ul> => {
+  const rows = data.map(({ wins, time, name, color, type }, index) => {
+    const car = new Car({ name, color, type });
+
+    const indexColumn = li({
+      className: styles.tableColumn,
+      text: (index + 1).toString(),
+    });
+    indexColumn.toggleClass(styles.indexColumn);
+
+    const carColumn = li({ className: styles.tableColumn }, car.wrapper);
+
+    const nameColumn = li({ className: styles.tableColumn, text: name });
+    nameColumn.toggleClass(styles.nameColumn);
+
+    const timeColumn = li({ className: styles.tableColumn, text: time.toString() });
+
+    const winsColumn = li({ className: styles.tableColumn, text: wins.toString() });
+
+    return ul(
+      { className: styles.tableRow },
+      indexColumn,
+      carColumn,
+      nameColumn,
+      winsColumn,
+      timeColumn
+    );
+  });
+
+  const content = ul({ className: styles.tableContent }, ...rows);
+
+  return content;
+};
+
+const createWinnersTableView = (): TableViewReturnType => {
+  const indexColumn = li({ className: styles.tableColumn, text: HeaderColumnText.Index });
+  indexColumn.toggleClass(styles.indexColumn);
+
+  const carColumn = li({ className: styles.tableColumn, text: HeaderColumnText.Car });
+
+  const nameColumn = li({ className: styles.tableColumn, text: HeaderColumnText.Name });
+  nameColumn.toggleClass(styles.nameColumn);
+
+  const winsColumn = new SortableColumn(
+    {
+      className: styles.sortableColumn,
+    },
+    span({ text: HeaderColumnText.Wins })
+  );
+  winsColumn.toggleClass(styles.tableColumn);
+
+  const timeColumn = new SortableColumn(
+    {
+      className: styles.sortableColumn,
+    },
+    span({ text: HeaderColumnText.BestTime })
+  );
+  timeColumn.toggleClass(styles.tableColumn);
+
+  const headerRow = ul(
+    { className: styles.headerRow },
+    indexColumn,
+    carColumn,
+    nameColumn,
+    winsColumn,
+    timeColumn
+  );
+
+  const tableContentWrapper = div();
+  const tableWrapper = div({ className: styles.table }, headerRow, tableContentWrapper);
+
+  return { winsColumn, timeColumn, tableContentWrapper, tableWrapper };
+};
+
+export const createView = (): CreateViewReturnType => {
+  const wrapper = div({ className: styles.wrapper });
+  const header = div({ className: styles.header });
+
+  const winnersCounter = span();
+  const winnersCounterWrapper = div(
+    { className: styles.winnersCounterWrapper },
+    span({ text: TOTAL_WINNERS_TEXT }),
+    winnersCounter
+  );
+
+  const paginator = new Paginator();
+
+  header.append(winnersCounterWrapper, paginator);
+
+  const { winsColumn, timeColumn, tableContentWrapper, tableWrapper } = createWinnersTableView();
+
+  wrapper.append(header, tableWrapper);
+
+  return {
+    wrapper,
+    paginator,
+    winnersCounter,
+    winsColumn,
+    timeColumn,
+    tableContentWrapper,
+  };
+};
---
+++ async-race/src/sections/winners/winners.ts
@@ -0,0 +1,124 @@
+import { Element } from '../../components/base/element.ts';
+import type { Paginator } from '../../components/paginator/paginator.ts';
+import type {
+  SortableColumn,
+  SortOrder,
+} from '../../components/sortable-column/sortable-column.ts';
+import { EventType } from '../../constants/index.ts';
+import * as api from '../../services/api/index.ts';
+import type { WinnerData } from '../../services/api/types.ts';
+import type { WinnerTableRowData } from './utils/create-view.ts';
+import { createView, createWinnersTableContent } from './utils/create-view.ts';
+
+const WINNERS_PER_PAGE = 10;
+const DEFAULT_PAGE_NUMBER = 1;
+const INITIAL_SORT_ORDER = 'asc';
+const INITIAL_SORT_COLUMN = 'wins';
+
+export class Winners extends Element {
+  private paginator: Paginator;
+  private winnersCounter: Element<HTMLSpanElement>;
+  private winsColumn: SortableColumn;
+  private timeColumn: SortableColumn;
+  private tableContentWrapper: Element<HTMLDivElement>;
+  private sortOrder: SortOrder = INITIAL_SORT_ORDER;
+  private sortColumn: 'wins' | 'time' = INITIAL_SORT_COLUMN;
+
+  constructor() {
+    super({ tag: 'section' });
+
+    const { wrapper, paginator, winnersCounter, winsColumn, timeColumn, tableContentWrapper } =
+      createView();
+
+    this.paginator = paginator;
+    paginator.itemsPerPage = WINNERS_PER_PAGE;
+
+    this.winnersCounter = winnersCounter;
+    this.winsColumn = winsColumn;
+    this.timeColumn = timeColumn;
+    this.tableContentWrapper = tableContentWrapper;
+
+    this.append(wrapper);
+    this.init();
+
+    void this.fetchWinnersData();
+  }
+
+  private async fetchWinnersData(pageNumber?: number): Promise<void> {
+    pageNumber = pageNumber || DEFAULT_PAGE_NUMBER;
+
+    const { items, totalCount } = await api.getAllWinners({
+      _limit: WINNERS_PER_PAGE,
+      _page: pageNumber,
+      _sort: this.sortColumn,
+      _order: this.sortOrder,
+    });
+    this.winnersCounter.text = totalCount.toString();
+    this.paginator.totalItems = totalCount;
+    this.paginator.currentPage = pageNumber;
+    void this.updateTableContent(items);
+  }
+
+  private async updateTableContent(items: WinnerData[]): Promise<void> {
+    const rowsData: WinnerTableRowData[] = [];
+    const carData = await Promise.all(items.map(({ id }) => api.getCar(id)));
+
+    items.forEach((winnerData, index) => {
+      if (carData[index]) {
+        rowsData.push({ ...winnerData, ...carData[index] });
+      }
+    });
+    // for (const winnerData of items) {
+    //   const carData = await api.getCar(winnerData.id);
+    //   if (carData) {
+    //     rowsData.push({ ...winnerData, ...carData });
+    //   }
+    // }
+    const content = createWinnersTableContent(rowsData);
+    this.tableContentWrapper.removeChildren();
+    this.tableContentWrapper.append(content);
+  }
+
+  private sortWinners = (order: SortOrder): void => {
+    this.sortOrder = order;
+    void this.fetchWinnersData(this.paginator.currentPage);
+  };
+
+  private addWinsColumnChangeHandler(): void {
+    this.winsColumn.onChange = (order: SortOrder): void => {
+      this.sortColumn = 'wins';
+      this.timeColumn.hideMarker();
+      this.sortWinners(order);
+    };
+  }
+
+  private addTimeColumnChangeHandler(): void {
+    this.timeColumn.onChange = (order: SortOrder): void => {
+      this.sortColumn = 'time';
+      this.winsColumn.hideMarker();
+      this.sortWinners(order);
+    };
+  }
+
+  private addOnBeforeContentChangeHandler(): void {
+    document.addEventListener(EventType.BeforeNavigateWinners, () => {
+      void this.fetchWinnersData(this.paginator.currentPage);
+    });
+  }
+
+  private addPaginatorChangeHandler(): void {
+    this.paginator.onChange = (newPage): void => {
+      void this.fetchWinnersData(newPage);
+    };
+  }
+
+  private init(): void {
+    this.winsColumn.sortOrder = INITIAL_SORT_ORDER;
+    this.timeColumn.hideMarker();
+
+    this.addTimeColumnChangeHandler();
+    this.addWinsColumnChangeHandler();
+    this.addOnBeforeContentChangeHandler();
+    this.addPaginatorChangeHandler();
+  }
+}
