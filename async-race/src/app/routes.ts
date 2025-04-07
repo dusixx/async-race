57191164cda4ffa5d05bd73ae218d58824cbc505
dusixx <113414935+dusixx@users.noreply.github.com>
2025-04-07 10:53:30 +0300

    feat: update routes

--- async-race/src/app/routes.ts
+++ async-race/src/app/routes.ts
@@ -1,7 +1,6 @@
 import type { Element } from '../components/base/element.ts';
 import type { Router } from '../router/router.ts';
 import { Garage } from '../sections/garage/garage.ts';
-import { NotFoundSection } from '../sections/index.ts';
 import { Winners } from '../sections/winners/winners.ts';
 
 export enum Pathname {
@@ -29,13 +28,5 @@ export const routes: Route[] = [
   {
     pathname: Pathname.Winners,
     component: () => new Winners(),
-  },
-  {
-    pathname: Pathname.Index,
-    component: (router: Router) => new NotFoundSection(router),
-  },
-  {
-    pathname: Pathname.NotFound,
-    component: (router: Router) => new NotFoundSection(router),
   },
 ];
