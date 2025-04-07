9fddf7cef4abf4ab87204d0beac7ef6e2adfa848
dusixx <113414935+dusixx@users.noreply.github.com>
2025-04-07 10:41:29 +0300

    feat: implement SortableColumn

---
+++ async-race/src/components/sortable-column/sortable-column.ts
@@ -0,0 +1,57 @@
+import { Icon, Visibility } from '../../constants/index.ts';
+import type { BaseElementProps } from '../base/create-element.ts';
+import { Element } from '../base/element.ts';
+import { span } from '../base/tags.ts';
+
+export type SortOrder = 'asc' | 'desc';
+
+type Props = Omit<BaseElementProps<HTMLLIElement>, 'tag'>;
+
+export class SortableColumn extends Element<HTMLLIElement> {
+  public onChange: ((order: SortOrder) => void) | null = null;
+  private _sortOrder: SortOrder = 'asc';
+  private marker: ReturnType<typeof span>;
+
+  constructor(props?: Props, ...children: (Element | null)[]) {
+    super({ tag: 'li', ...props }, ...children);
+
+    this.marker = span({ text: Icon.ArrowDown });
+    this.append(this.marker);
+
+    this.init();
+  }
+
+  public get sortOrder(): SortOrder {
+    return this._sortOrder;
+  }
+
+  private get isMarkerVisible(): boolean {
+    return this.marker.node.style.visibility !== Visibility.Hidden.toString();
+  }
+
+  public set sortOrder(order: SortOrder) {
+    this._sortOrder = order;
+    this.marker.text = order === 'asc' ? Icon.ArrowDown : Icon.ArrowUp;
+    this.onChange?.(order);
+  }
+
+  public hideMarker(): void {
+    this.showMarker(false);
+  }
+
+  private showMarker(flag: boolean): void {
+    this.marker.node.style.visibility = flag ? '' : Visibility.Hidden;
+  }
+
+  private init(): void {
+    this.addListener('click', () => {
+      // reveal on click and keep order as is
+      if (!this.isMarkerVisible) {
+        this.showMarker(true);
+        this.onChange?.(this.sortOrder);
+        // return;
+      }
+      this.sortOrder = this.sortOrder === 'asc' ? 'desc' : 'asc';
+    });
+  }
+}
