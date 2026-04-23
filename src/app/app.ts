3d1aec30f54a1aa0fc97ef8e6d5791b12a6b1ee7
dusixx <113414935+dusixx@users.noreply.github.com>
2026-04-23 21:04:38 +0300

    feat: update app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,11 @@
 import { EventName } from '@common';
 import type { NavSectionName } from '@components';
 import { Garage, HeaderSection, main as MainSection, Winners } from '@components';
-import { showStartMessageOnce } from './app.utils.ts';
 
 const header = new HeaderSection();
 const garage = new Garage();
 const winners = new Winners();
 const main = MainSection({}, garage);
-
-document.addEventListener('DOMContentLoaded', () => {
-  showStartMessageOnce();
-});
 
 header.onNavigate = (sectionName: NavSectionName): void => {
   main.dispatch(EventName.BeforeContentChange);
